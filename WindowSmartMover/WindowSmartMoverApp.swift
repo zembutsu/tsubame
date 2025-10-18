59d6b6be9933df7d36ebc8725a8c21439c543e9f
Masahito Zembutsu <m.zembutsu@gmail.com>
2025-10-18 09:46:12 +0900

    Initial Commit

---
+++ WindowSmartMover/WindowSmartMoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSmartMoverApp.swift
+//  WindowSmartMover
+//
+//  Created by Masahito Zembutsu on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct WindowSmartMoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
